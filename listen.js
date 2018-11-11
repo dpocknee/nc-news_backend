41e7acae3b819503c11581e6f14235ed4de33768
dpocknee <dappy_iodo@hotmail.com>
2018-11-11 15:41:47 +0000

    changed PORT error.

--- listen.js
+++ listen.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 const { PORT = 9090 } = process.env;
 
-app.listen(9090, err => {
+app.listen(PORT, err => {
   if (err) throw err;
   console.log(`listening on port ${PORT}`);
 });
