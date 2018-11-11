a4e298e447e91c32acbf4202a764ae14cc90345a
dpocknee <dappy_iodo@hotmail.com>
2018-11-11 14:02:08 +0000

    setting up mlab and heroku.

--- seed/runSeed.js
+++ seed/runSeed.js
@@ -1,7 +1,8 @@
 // If seed:dev or seed:test is called from the terminal:
-const seedType = process.env.npm_lifecycle_event.split(':')[1];
 // If the ENV is set by some code:
-const currentEnv = process.env.NODE_ENV ? process.env.NODE_ENV : seedType;
+const currentEnv = process.env.NODE_ENV
+  ? process.env.NODE_ENV
+  : process.env.npm_lifecycle_event.split(':')[1];
 
 const createSeed = require('./createSeed');
 const mongoose = require('mongoose');
